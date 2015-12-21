076645d134e1770a6ef85169e51af6df1f5e0f99
Eugen Bryuhachev <novikovpyotr@gmail.com>
2015-12-21 21:43:08 +0700

    Created empty project, added UINavigationBar and customed that

---
+++ Social App Prototype/Social App Prototype/ColorConstants.swift
@@ -0,0 +1,9 @@
+//
+//  ColorConstants.swift
+//  Social App Prototype
+//
+//  Created by Eugen Briukhachyov on 21.12.15.
+//  Copyright © 2015 Eugen Briukhachyov. All rights reserved.
+//
+
+import Foundation
---
+++ Social App Prototype/Social App Prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social App Prototype
+//
+//  Created by Eugen Briukhachyov on 21.12.15.
+//  Copyright © 2015 Eugen Briukhachyov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
