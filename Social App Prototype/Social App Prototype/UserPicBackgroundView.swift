62764afa486506e6539478f9bb1256bf7ccd48b5
Eugen Bryuhachev <novikovpyotr@gmail.com>
2016-01-08 00:24:24 +0700

    fix visual difference between prototype and implementation. user info block added

---
+++ Social App Prototype/Social App Prototype/UserPicBackgroundView.swift
@@ -0,0 +1,21 @@
+//
+//  UserPicBackgroundView.swift
+//  Social App Prototype
+//
+//  Created by Eugen Briukhachyov on 07.01.16.
+//  Copyright © 2016 Eugen Briukhachyov. All rights reserved.
+//
+
+import UIKit
+
+class UserPicBackgroundView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
